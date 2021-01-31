2d4e0ee050e5941f261620d12653fea247f53dfc
rawlin <rawlincrasto@gmail.com>
2021-01-31 18:42:16 +0530

    layouts for details

---
+++ app/src/main/java/com/example/musicwiki/ui/albumdetails/AlbumDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musicwiki.ui.albumdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.musicwiki.R
+
+class AlbumDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album_details)
+    }
+}
---
+++ app/src/main/java/com/example/musicwiki/ui/artistdetails/ArtistDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musicwiki.ui.artistdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.musicwiki.R
+
+class ArtistDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist_details)
+    }
+}
