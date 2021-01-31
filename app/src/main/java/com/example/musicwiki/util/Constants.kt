4c21f603762cd1a8c1908e8a64d175df1eaae114
rawlin <rawlincrasto@gmail.com>
2021-02-01 02:11:10 +0530

    some changes

---
+++ app/src/main/java/com/example/musicwiki/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.musicwiki.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "b5c10d9c6af908a4a015a5e4cbef1c1c"
+        const val BASE_URL = "https://ws.audioscrobbler.com"
+    }
+}
